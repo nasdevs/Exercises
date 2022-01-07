d8c4bf7dbcadeb67fdf686f23d5a731a39156a64
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2022-01-07 08:45:51 +0800

    Update solution.py

--- 2nd/solution.py
+++ 2nd/solution.py
@@ -1,4 +1,10 @@
+'''
+@name : exercise 2nd
+@author : Nasrullah
+@url github : https://github.com/nasdevs
+@url repository : https://github.com/nasdevs/exercises-and-solution
 limit = int(input('Limit : '))
+'''
 
 for i in range(1, limit+1):
     for j in range(0, i):
