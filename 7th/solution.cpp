75227f8d6d38428f8d4c15a14d6074beadf030ab
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2022-01-25 22:10:19 +0800

    add solution

---
+++ 7th/solution.cpp
@@ -0,0 +1,15 @@
+/*
+@name : exercise 7th
+@url github : https://github.com/nasdevs
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string N;
+    cin >> N;
+
+    string result = (N.find('0') == true ? "YES" : "NO");
+    cout << result << "\n";
+}
