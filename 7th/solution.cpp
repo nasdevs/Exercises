3b4b3111b0fa84c8422b931848998af7a4ab83f5
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2022-01-25 22:18:52 +0800

    Update solution.cpp

--- 7th/solution.cpp
+++ 7th/solution.cpp
@@ -10,6 +10,6 @@ int main() {
     string N;
     cin >> N;
 
-    string result = (N.find('0') == true ? "YES" : "NO");
+    string result = (N.find('0') == string::npos ? "NO" : "YES"); // string::npos: It is a constant static member value with the highest possible value for an element of type size_t. It actually means until the end of the string. It is used as the value for a length parameter in the string's member functions. As a return value, it is usually used to indicate no matches
     cout << result << "\n";
 }
